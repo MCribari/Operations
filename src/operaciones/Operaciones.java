a77b8dd1db2a4b2c2d69c35b79bcb041972377d8
Mateo <mateocribari05@gmail.com>
2025-04-16 11:36:31 -0300

    Initial commit

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,26 @@
+package operaciones;
+
+public class Operaciones {
+    public static void main(String[] args) {
+
+        double A = 26.0/4;
+        double B = 78.5/2;
+        double C = 105.0/75;
+        int D = (0==1)?(45/0):0;
+        int E = 28*4;
+        int F = (78)-(-12)+(-15);
+        int G = 10/5+(-3)+1;
+        double H = 16/3.0+16/5;
+
+        System.out.println("A. "+ A);
+        System.out.println("B. "+ B);
+        System.out.println("C. "+ C);
+        System.out.println("D. "+ D);
+        System.out.println("E. "+ E);
+        System.out.println("F. "+ F);
+        System.out.println("G. "+ G);
+        System.out.println("H. "+ H);
+
+    }
+
+}
